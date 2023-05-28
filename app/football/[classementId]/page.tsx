e2ee907ff97bbd17a9408098b1fca2562b96eee5
Melvin SILVA <melvin.silva@sncf.fr>
2023-05-28 16:50:08 +0200

    test deploy 8

--- app/football/[classementId]/page.tsx
+++ app/football/[classementId]/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
 import ClassementTable from "../classementTable/page";
-import ButeursTableComponent from "../buteurTable/page";
+import ButeursTableComponent from "../buteurTable/pagebuteurs";
 
 interface Team {
   TEAM_ID: number;
