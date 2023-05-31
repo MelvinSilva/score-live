403a899a4ac1f9a4ef4754159900d30e3b64220c
Melvin SILVA <melvin.silva@sncf.fr>
2023-05-31 16:36:26 +0200

    test2

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ["www.flashscore.com", "cdn.dribbble.com"],
   },
+  env: { NEXT_PUBLIC_X_RAPIDAPI_KEY: process.env.NEXT_PUBLIC_X_RAPIDAPI_KEY },
 };
 
 module.exports = nextConfig;
