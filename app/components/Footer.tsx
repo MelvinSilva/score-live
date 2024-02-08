2331fe8d89352968fec3c2a20104a93cc842be8d
Melvin SILVA <melvin.silva@sncf.fr>
2024-02-08 23:39:21 +0100

    update url portfolio

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,16 +5,16 @@ export default function Footer() {
     <div>
       <footer className="flex flex-col items-center justify-center w-full py-4 bg-gradient-to-r from-denim-600 to-denim-900 shadow-md mb-12 lg:mb-0 xl:mb-0 2xl:mb-0 ">
         <p className="text-gray-200">
-          © 2023 Sporty Score - Tous droits réservés
+          © 2024 Sporty Score - Tous droits réservés
         </p>
         <a
-          href="https://melvinsilva.tech/"
+          href="https://melvinsilva.vercel.app/"
           className="text-denim-300 font-semibold text-md"
         >
           <span className="text-gray-200">Réalisé par</span>&nbsp;Melvin Silva
         </a>
         <a
-          href="https://melvinsilva.tech/"
+          href="https://melvinsilva.vercel.app/"
           className="text-denim-300 text-xs mt-3"
         >
           <span className="text-gray-200">Mon portfolio ☛</span>
